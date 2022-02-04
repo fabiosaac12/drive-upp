7921d8c82b6228dc6a7df0919ef48b92062d1a07
Fabio Isaac <fabiosaac12@gmail.com>
2022-02-03 21:24:59 -0400

    feat: app name

--- android/app/src/main/java/com/driveupp/MainApplication.java
+++ android/app/src/main/java/com/driveupp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.reactnativearchitecturets;
+package com.driveupp;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.reactnativearchitecturets.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.driveupp.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
--- ios/DriveUppTests/DriveUppTests.m
+++ ios/DriveUppTests/DriveUppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ReactNativeArchitectureTSTests : XCTestCase
+@interface DriveUppTests : XCTestCase
 
 @end
 
-@implementation ReactNativeArchitectureTSTests
+@implementation DriveUppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
