fe5af9b3cda1041d687df8c31967e4d901ac6580
Fabio Isaac <fabiosaac12@gmail.com>
2022-02-08 08:06:40 -0400

    feat: handling when there are no mechanics

---
+++ src/providers/UserAssistance/UserAssistanceMessages.ts
@@ -0,0 +1,12 @@
+import { defineMessages } from 'providers/Language';
+
+export const useMessages = defineMessages({
+  noMechanicAvailable: {
+    en: 'For now, there are no mechanics available. Try again later.',
+    es: 'Por ahora, ho nay mecanicos disponibles. Intenta nuevamente.',
+  },
+  accept: {
+    en: 'Accept',
+    es: 'Aceptar',
+  },
+});
--- src/providers/UserAssistance/UserAssistanceProvider.tsx
+++ src/providers/UserAssistance/UserAssistanceProvider.tsx
@@ -1,25 +1,44 @@
+import { InfoModal } from 'components/InfoModal';
 import { useLocation } from 'providers/Location';
+import { useModal } from 'providers/Modal';
 import { useSocket } from 'providers/Socket';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FC } from 'react';
 import { Status } from './models/Status';
 import {
   UserAssistanceContext,
   UserAssistanceContextProps,
 } from './UserAssistanceContext';
+import { useMessages } from './UserAssistanceMessages';
 
 export const UserAssistanceProvider: FC = ({ children }) => {
+  const modal = useModal();
   const { socket } = useSocket();
   const { locationRef } = useLocation();
+  const messages = useMessages();
   const [status, setStatus] = useState<Status>('inactive');
+
+  useEffect(() => {
+    socket.on('help_no_mechanic_available', () => {
+      setStatus('inactive');
+      modal.handleOpen({
+        content: (
+          <InfoModal
+            variant="secondary"
+            title={messages.noMechanicAvailable}
+            buttonText={messages.accept}
+          />
+        ),
+      });
+    });
+
+    return () => {
+      socket.off('help_no_mechanic_available');
+    };
+  });
 
   const searchForHelp = () => {
     if (locationRef.current) {
-      console.log({
-        lat: locationRef.current.latitude,
-        lng: locationRef.current.longitude,
-      });
-
       socket.emit('search_help', {
         lat: locationRef.current.latitude,
         lng: locationRef.current.longitude,
