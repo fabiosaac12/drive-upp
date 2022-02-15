b49f8c3e58478e9ea461241a8cf2babdc95202ba
Fabio Isaac <fabiosaac12@gmail.com>
2022-02-15 17:35:40 -0400

    feat: loader when location is not available yet

--- src/screens/NoLocationMapScreen/NoLocationMapScreen.tsx
+++ src/screens/NoLocationMapScreen/NoLocationMapScreen.tsx
@@ -5,11 +5,13 @@ import { useMessages } from './NoLocationMapScreenMessages';
 import { useStyles } from './NoLocationMapScreenStyles';
 import { useTheme } from 'providers/Theme';
 import { Text } from 'components/Text';
+import { useLocation } from 'providers/Location';
 
 export const NoLocationMapScreen = withLayout(() => {
   const { theme } = useTheme();
   const messages = useMessages();
   const styles = useStyles();
+  const location = useLocation();
 
   return (
     <View style={styles.container}>
@@ -18,9 +20,13 @@ export const NoLocationMapScreen = withLayout(() => {
       </View>
 
       <View style={styles.contentContainer}>
-        <Text variant="subtitle" color="secondary" style={styles.message}>
-          {messages.message}
-        </Text>
+        {location.enabled === false ? (
+          <Text variant="subtitle" color="secondary" style={styles.message}>
+            {messages.message}
+          </Text>
+        ) : (
+          <ActivityIndicator size="large" color={theme.palette.primary.main} />
+        )}
       </View>
     </View>
   );
